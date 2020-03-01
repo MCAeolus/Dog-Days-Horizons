fff479f66884097655aa32bc9d7add60ab7784ba
Nathan Smith <mcaeolusch@gmail.com>
2020-02-29 20:17:06 -0500

    init 3

--- MainScreen.gd
+++ MainScreen.gd
@@ -5,13 +5,17 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+
 onready var stat = get_node("/root/Tracker")
+onready var anim = $AnimationPlayer
+
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_SPACE):
 		stat.nextWorld()
 		
-
+func _ready():
+	anim.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
